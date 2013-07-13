b7e2308304b774963523b939d1fce3422a4dfe5f
Vazkii <vasco.men@hotmail.com>
2013-07-13 01:48:29 +0100

    Initial commit with base mod file. woot.

---
+++ MODSRC/vazkii/kap/KingsAndPeasants.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the Kings and Peasants Mod.
+ * 
+ * Kings and Peasants is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [13 Jul 2013, 01:26:49 (GMT)]
+ */
+package vazkii.kap;
+
+import vazkii.kap.core.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public final class KingsAndPeasants {
+
+}
---
+++ MODSRC/vazkii/kap/core/lib/LibMisc.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the Kings and Peasants Mod.
+ * 
+ * Kings and Peasants is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [13 Jul 2013, 01:28:33 (GMT)]
+ */
+package vazkii.kap.core.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "KingsAndPeasants";
+	public static final String MOD_NAME = "Of Kings And Peasants";
+	public static final String VERSION = "@VERSION@";
+	
+}
